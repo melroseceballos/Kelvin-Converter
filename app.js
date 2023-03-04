a5cef526a1344302b183f4ac05573a128a531bb6
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-03-03 23:31:38 -0800

    re-touched js

---
+++ app.js
@@ -0,0 +1,9 @@
+// kelvin value
+const kelvin = 1
+// celsius value
+const celsius = kelvin - 273;
+// fahrenheit value
+let fahrenheit = celsius * (9/5) + 32
+fahrenheit = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
